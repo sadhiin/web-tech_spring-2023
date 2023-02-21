a0cb46927573739b7ca6a5f3ff7f13131ccc8ea1
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-02-21 14:08:56 +0600

    lab - 4 starting

---
+++ Lab-4-task/A.php
@@ -0,0 +1,13 @@
+<?php
+    $pageTitle = "Public Home page";
+    include "./include/header.php";
+
+    echo "<h1>Welcome to the Homepage</h1>";
+
+
+
+    
+    require_once("./include/footer.php")
+
+?>
+
---
+++ Lab-4-task/include/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle ?></title>
+    <style>
+        /* Style the topnav links */
+
+
+
+        /* Style the header */
+        .header {
+            background-color: #f1f1f1;
+            padding: 20px;
+            text-align: center;
+        }
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }   
+
+        /* style for footer */
+        .footer {
+            align-items: center;
+            border: 1px solid;
+            padding-left: 650px;
+            justify-content: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="topbar">
+        <img src="../images/logo_icon.ico" alt="">
+        <div class="topanv">
+            <a href="A.php">Home Page</a>
+            <a href="Login.php">Login</a>
+            <a href="Registation.php">Registation</a>
+        </div>
+    </div>
